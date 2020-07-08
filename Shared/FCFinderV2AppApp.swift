13d2f69a07e3330db93c94aced52986a1f642c63
Brad Leege <bleege@gmail.com>
2020-07-07 19:08:37 -0500

    Initial Commit

---
+++ Shared/FCFinderV2AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FCFinderV2AppApp.swift
+//  Shared
+//
+//  Created by Brad Leege on 7/7/20.
+//
+
+import SwiftUI
+
+@main
+struct FCFinderV2AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
