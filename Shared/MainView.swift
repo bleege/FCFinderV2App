bdb0a0b68946b1954131cdfc9233eb4bbed82ead
Brad Leege <bleege@gmail.com>
2020-07-14 21:16:42 -0500

    Full Screen MainView with Map

--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -8,19 +8,17 @@
 import SwiftUI
 import MapKit
 
-struct ContentView: View {
+struct MainView: View {
     
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 43.07472, longitude: -89.38421), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5 ))
     
     var body: some View {
-        VStack {
-            Map(coordinateRegion: $region)
-        }
+        Map(coordinateRegion: $region).edgesIgnoringSafeArea(.all)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
