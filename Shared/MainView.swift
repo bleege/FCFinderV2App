368f4f3955d82c4eab2cb3ee96bb80ac1a719298
Brad Leege <bleege@gmail.com>
2020-07-14 22:36:56 -0500

    Stubbing out Picker UI

--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 import MapKit
 
+enum Flavor: String, CaseIterable, Identifiable {
+    case chocolate
+    case vanilla
+    case strawberry
+
+    var id: String { self.rawValue }
+}
+
 struct MainView: View {
     
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 43.07472, longitude: -89.38421), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5 ))
     @State private var bottomSheetShown = false
+    @State private var selectedFlavor = Flavor.chocolate
     
     var body: some View {
 
         GeometryReader { geometry in
             Map(coordinateRegion: $region)
             BottomSheetView(isOpen: $bottomSheetShown, maxHeight: geometry.size.height * 0.7) {
-                Text("Selected League Name Goes Here")
+                VStack {
+                    Text("Selected League Name Goes Here")
+                    Picker("Flavor 1", selection: $selectedFlavor) {
+                        ForEach(Flavor.allCases) { flavor in
+                            Text(flavor.rawValue.capitalized)
+                        }
+                    }
+                    Picker("Flavor 2", selection: $selectedFlavor) {
+                        ForEach(Flavor.allCases) { flavor in
+                            Text(flavor.rawValue.capitalized)
+                        }
+                    }
+                    Picker("Flavor 3", selection: $selectedFlavor) {
+                        ForEach(Flavor.allCases) { flavor in
+                            Text(flavor.rawValue.capitalized)
+                        }
+                    }
+                }
             }
         }.edgesIgnoringSafeArea(.all)
         
