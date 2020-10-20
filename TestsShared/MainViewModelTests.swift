bc748f7e47e353981c6cb279b2a576154eba891c
Brad Leege <bleege@gmail.com>
2020-10-19 21:06:42 -0500

    Building out tests

--- TestsShared/MainViewModelTests.swift
+++ TestsShared/MainViewModelTests.swift
@@ -20,4 +20,39 @@ class MainViewModelTests: XCTestCase {
         XCTAssertEqual(country.name, "United States")
     }
 
+    func testGetLeaguesByCountry() throws {
+        let dataManagerMock = DataManagerMock()
+        let viewModel = MainViewModel(dataManagerMock)
+        viewModel.getLeaguesByCountry(countryId: 1)
+        XCTAssertEqual(viewModel.countryLeagues.count, 1)
+        let league = viewModel.countryLeagues[0]
+        XCTAssertEqual(league.leagueId, 1)
+        XCTAssertEqual(league.name, "MLS")
+        XCTAssertEqual(league.country.countryId, 1)
+        XCTAssertEqual(league.country.name, "United States")
+        XCTAssertEqual(league.confederation, "CONCACAF")
+    }
+
+    func testGetYearsForLeague() throws {
+        let dataManagerMock = DataManagerMock()
+        let viewModel = MainViewModel(dataManagerMock)
+        viewModel.getYearsForLeague(leagueId: 1)
+        XCTAssertEqual(viewModel.leagueYears.count, 2)
+        XCTAssertEqual(viewModel.leagueYears[0], 2020)
+        XCTAssertEqual(viewModel.leagueYears[1], 2021)
+    }
+    
+    func testGetClubsForLeagueAndYear() throws {
+        let dataManagerMock = DataManagerMock()
+        let viewModel = MainViewModel(dataManagerMock)
+        viewModel.getClubsForLeagueAndYear(leagueId: 1, year: 2020)
+        XCTAssertEqual(viewModel.clubs.count, 1)
+        let club = viewModel.clubs[0]
+        XCTAssertEqual(club.clubId, 1)
+        XCTAssertEqual(club.name, "Seattle Sounders")
+        XCTAssertEqual(club.stadiumName, "CenturyLink Field")
+        XCTAssertEqual(club.latitude, 47.59506)
+        XCTAssertEqual(club.longitude, -122.33163)
+    }
+    
 }
